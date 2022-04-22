85dc4009ebe82f4b2f6c57b0475dcd09c52c5881
cujo <cujothechief@gmail.com>
2022-04-22 03:07:09 +0000

    Updates

---
+++ precache-manifest.976d7ac9f5be6ee9424f923601a1e345.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "e388d5cbc55a7822f516",
+    "url": "/social-media-dashboard/static/css/main.a81df4ba.chunk.css"
+  },
+  {
+    "revision": "e388d5cbc55a7822f516",
+    "url": "/social-media-dashboard/static/js/main.e388d5cb.chunk.js"
+  },
+  {
+    "revision": "f4eb7e48bd10ac4e2578",
+    "url": "/social-media-dashboard/static/js/1.f4eb7e48.chunk.js"
+  },
+  {
+    "revision": "f3819348da8f91b5907e",
+    "url": "/social-media-dashboard/static/js/runtime~main.f3819348.js"
+  },
+  {
+    "revision": "95f916a549511a0912328d5626522d2c",
+    "url": "/social-media-dashboard/static/media/icon-up.95f916a5.svg"
+  },
+  {
+    "revision": "69a5c77f51678c145d64af371ff73b22",
+    "url": "/social-media-dashboard/static/media/icon-down.69a5c77f.svg"
+  },
+  {
+    "revision": "a738376499ac8a902fd2ce03c3030f98",
+    "url": "/social-media-dashboard/static/media/icon-facebook.a7383764.svg"
+  },
+  {
+    "revision": "810ccee84b0132ca7921a1b0b4ee6021",
+    "url": "/social-media-dashboard/static/media/icon-twitter.810ccee8.svg"
+  },
+  {
+    "revision": "991487fe03dace6f69a870d3085aadf1",
+    "url": "/social-media-dashboard/static/media/icon-instagram.991487fe.svg"
+  },
+  {
+    "revision": "8499da129aedb70f3e20262951f7e236",
+    "url": "/social-media-dashboard/static/media/icon-youtube.8499da12.svg"
+  },
+  {
+    "revision": "6aac34eaa1f7b32512f7f68d930d9bdf",
+    "url": "/social-media-dashboard/index.html"
+  }
+];
